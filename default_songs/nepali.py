806bce54910ff75f0e093ff4bd510d06f8ec5ff7
Prakash Aryal <aryalprakas@gmail.com>
2017-04-18 22:08:44 +0545

    nepali :P

---
+++ default_songs/nepali.py
@@ -0,0 +1,372 @@
+__author__ = 'prakash'
+
+nepali = [
+    {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=szbEDqhOrGY",
+        "tags": ["nepali"]
+    },
+    {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=lb77E2Uk4dg",
+        "tags": ["nepali"]
+    },
+    {
+
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=S8-CzAHk1-o",
+        "tags": ["nepali"]
+        },
+        {
+
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=4R7BxxuGmmE",
+        "tags": ["nepali"]
+
+        },
+        {
+
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=Arfujgk7R3Y",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=e5kYmiJeioQ",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=x-92M395Qxk",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=v5A8F2WsrC4",
+        "tags":["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=JXzswyfFg70",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=AwiP3vxYyZE",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=VolQAtmNPbo",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=FXxNoN5ajfQ",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=CF07tyEh5vA",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=D04_6CKaOhc",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=T-loYVTrGA0",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=L3EptHMYoMQ",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=WsHXSExXIlo",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=rBTH8p0HwRA",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=cCLKQigs7-4",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=cmqrTyB200w",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=i4BK3fvyC34",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=QiCMpH6q1po",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=0FsK_xosi9g",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=Yru9O1SLvfs",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=0ngMoOWVWuM",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=VEy55gCszsM",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=BaM9fql3PnI",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=pwTfUhPSmHE",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=zIpobsaVY6o",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=_VwbA3fAQg8",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=8N2VIrej44M",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name": "Resham TV",
+        "message": "",
+        "youtube": "https://www.youtube.com/watch?v=joCqaqEozAg",
+        "tags": ["nepali"]
+        },
+
+        {
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=utAHmLMetFI",
+        "tags":["nepali"]
+        },
+
+        {
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=uIszV-QnpX4",
+        "tags":["nepali"]
+        },
+
+        {
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=7CdnrwLVcII",
+        "tags":["nepali"]
+        },
+
+        {
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=Yc_791gQzYg",
+        "tags":["nepali"]
+        },
+
+        {
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=uB6xkMzMimM",
+        "tags":["nepali"]
+        },
+
+        {
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=1Hw9vaYLwcc",
+        "tags":["nepali"]
+        },
+
+        {
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=g45A9m4IznY",
+        "tags":["nepali"]
+        },
+
+        {
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=5dKGK81z4js",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=ukohCVT6f5M",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=uW7k3sAW_nc",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=UtbnTW3du9Y",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=eWtiegDrtIY",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=4wBLQlwh_xg",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=R6Ht72w_BiE",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=3p4WKHVsQX8",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=Wg8lL8a4WEs",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=q3U_8Ul5Cnw",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=QJvwzbim4kg",
+        "tags":["nepali"]
+        },
+
+
+{
+        "name":"Resham TV",
+        "message":"",
+        "youtube":"https://www.youtube.com/watch?v=YJhXMJDrX_E",
+        "tags":["nepali"]
+        }
+
+]
